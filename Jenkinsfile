414f9427a80f5b156a851f56d9e68525180247f8
jdvalera <johnlemuelvalera@gmail.com>
2021-05-29 15:46:35 -0700

    Initialize Spring Boot Project

---
+++ Jenkinsfile
@@ -0,0 +1,29 @@
+  
+pipeline {
+	agent any
+	tools {
+    	maven 'local-mvn'
+	}
+	stages {
+    	stage("Checkout") {   
+        	steps {               	 
+            	git branch: 'main',  url: 'https://github.com/jdvalera/devops-project-3'          	 
+        	}    
+    	}
+    	stage('Build') {
+        	steps {
+        	sh "mvn compile"  	 
+        	}
+    	}
+    	stage("Unit test") {          	 
+        	steps {  	 
+            	sh "mvn test"          	 
+       	    }
+        }
+    	stage("Package") {          	 
+        	steps {  	 
+            	sh "mvn package"  	 
+       	    }
+        }
+    }
+}
---
+++ src/test/java/com/simplilearn/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,32 @@
+package com.simplilearn.restservice;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void testRootPath() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		final String baseUrl = "http://localhost:8080";
+	    URI uri;
+		try {
+			uri = new URI(baseUrl);
+			ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+			Assertions.assertEquals(200, result.getStatusCodeValue());
+			Assertions.assertEquals(true, result.getBody().contains("Hello there"));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			Assertions.fail("Error, could not locate URI");
+		}
+	}
+}
